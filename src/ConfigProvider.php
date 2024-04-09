d2d97a6da2dcb7e72f9a7dacf491870ac9b8fed0
wlfpanda <wlfpanda1012@gmail.com>
2024-04-09 17:11:56 +0800

    Initial commit

---
+++ src/ConfigProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Wlfpanda1012\SimpleJwtClassmap;
+
+use Qbhy\SimpleJwt\JWTManager;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+            ],
+            'commands' => [
+            ],
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                    'class_map' => [
+                        JWTManager::class => __DIR__ . '/ClassMap/JWTManager.php',
+                    ],
+                ],
+            ],
+        ];
+    }
+}
